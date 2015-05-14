d4f7853925987cbdb36b55fd54600f3ee6119473
Ng Kitt Horng, Zion <zion@intzone.com>
2015-05-14 20:58:56 +0800

    Start session manually

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
+
 // chdir(dirname(__DIR__)); // Not needed if index.php is in same directory as init_autoloader.php
 
 // Decline static file requests back to the PHP built-in webserver
@@ -11,7 +12,14 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
 }
 
 // Setup autoloading
-require 'init_autoloader.php';
+include 'init_autoloader.php';
 
-// Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+// Start session manually to prevent error below caused by FlashMessenger
+// "PHP Fatal error:  Class name must be a valid object or a string in Zend/Stdlib/ArrayObject.php on line 230"
+// Solution from https://github.com/zendframework/zf2/issues/4386#issuecomment-24896063
+$sessionManager = new Zend\Session\SessionManager();
+$sessionManager->setName('unique-name-for-app');
+$sessionManager->start();
+
+// Run application
+Zend\Mvc\Application::init(include 'config/application.config.php')->run();
